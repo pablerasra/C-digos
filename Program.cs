44d54a51a015332914d0b4bf43fd92fa6c209c9c
pablerasra <121519418+pablerasra@users.noreply.github.com>
2026-09-03 14:12:30 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Colección_Genérica_e_Iteración_Personalizada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
